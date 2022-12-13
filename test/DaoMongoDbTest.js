a3687c8ec1894116b6972246061b47c7b27ec157
Rafael Queiroz Gonçalves <rafael.q.g@hotmail.com>
2022-12-13 08:03:17 -0300

    adjust mocha tests

--- test/DaoMongoDbTest.js
+++ test/DaoMongoDbTest.js
@@ -1,6 +1,7 @@
 let DAOMongoDB = require ("../src/integration/DAOMongoDB");
 let assert = require('assert'); 
 const { checkPrime } = require("crypto");
+const { doesNotMatch} = require("assert");
 
 let daoMongoDB = new DAOMongoDB();
 
@@ -8,8 +9,9 @@ describe('DAOMongoDB', function () {
     describe('#getAllEmployees', function () {
         it('should return more than 0 documents', async function () { 
             let documents = await daoMongoDB.getAllEmployees();
+            console.log("documents", documents);
             assert.equal(documents.length > 0, true);
-        });
+        }).timeout(8000);
     });
 });
 
